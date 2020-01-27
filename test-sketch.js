77608137a0957d45b835935053aed793a13291ca
tracyw04 <39570002+tracyw04@users.noreply.github.com>
2020-01-26 16:48:01 -0800

    add test-sketch

--- test-sketch.js
+++ test-sketch.js
@@ -1,44 +1,120 @@
-/* Jan. 23, 2020 
-My goal is to create a green city on a blue background
-*/
+// Drawn from https://p5js.org/examples/simulate-penrose-tiles.html
+// This is a port by David Blitz of the "Penrose Tile" example from processing.org/examples
 
-//setup
+var ds;
+
 function setup() {
-  createCanvas(800, 800);
-  rectMode(CENTER);
+  createCanvas(710, 400);
+  ds = new PenroseLSystem();
+  //please, play around with the following line
+  ds.simulate(5);
 }
 
-//drawing fxn
 function draw() {
-// declaration of variables:allows to change where city & symbols are located on canvas
-var x = 200;
-var y = 200;
-  // line 1
-line(x, y+100, x, y+200);
-  stroke(75)
-  strokeWeight(2);
-  //line 2
-  line(x, y+100,x+350,y);
-  stroke(75)
-  strokeWeight(2);
-    //line 3
-  line(x, y+200,x+450,y+200);
-  stroke(75)
-  strokeWeight(2);
-  //line 4
-    line(x+350, y,x+450,y);
-  stroke(75)
-  strokeWeight(2);
-  //line 4
-    line(x+450, y,x+450,y+200);
-  stroke(75)
-  strokeWeight(2);
-  //circle 1
-  noStroke();
-  fill(255,53,139);
-  ellipse(x+100, y+100, 50, 50);
-    //circle 2
-  noStroke();
-  fill(255,53,139);
-  ellipse(x+300, y+100, 100, 100);
+  background(0);
+  ds.render();
 }
+
+function PenroseLSystem() {
+    this.steps = 0;
+
+   //these are axiom and rules for the penrose rhombus l-system
+   //a reference would be cool, but I couldn't find a good one
+    this.axiom = "[X]++[X]++[X]++[X]++[X]";
+    this.ruleW = "YF++ZF----XF[-YF----WF]++";
+    this.ruleX = "+YF--ZF[---WF--XF]+";
+    this.ruleY = "-WF++XF[+++YF++ZF]-";
+    this.ruleZ = "--YF++++WF[+ZF++++XF]--XF";
+
+    //please play around with the following two lines
+    this.startLength = 460.0;
+    this.theta = TWO_PI / 10.0; //36 degrees, try TWO_PI / 6.0, ...
+    this.reset();
+}
+
+PenroseLSystem.prototype.simulate = function (gen) {
+  while (this.getAge() < gen) {
+    this.iterate(this.production);
+  }
+}
+
+PenroseLSystem.prototype.reset = function () {
+    this.production = this.axiom;
+    this.drawLength = this.startLength;
+    this.generations = 0;
+  }
+
+PenroseLSystem.prototype.getAge = function () {
+    return this.generations;
+  }
+
+//apply substitution rules to create new iteration of production string
+PenroseLSystem.prototype.iterate = function() {
+    var newProduction = "";
+
+    for(var i=0; i<this.production.length; ++i) {
+      var step = this.production.charAt(i);
+      //if current character is 'W', replace current character
+      //by corresponding rule
+      if (step == 'W') {
+        newProduction = newProduction + this.ruleW;
+      }
+      else if (step == 'X') {
+        newProduction = newProduction + this.ruleX;
+      }
+      else if (step == 'Y') {
+        newProduction = newProduction + this.ruleY;
+      }
+      else if (step == 'Z') {
+        newProduction = newProduction + this.ruleZ;
+      }
+      else {
+        //drop all 'F' characters, don't touch other
+        //characters (i.e. '+', '-', '[', ']'
+        if (step != 'F') {
+          newProduction = newProduction + step;
+        }
+      }
+    }
+
+    this.drawLength = this.drawLength * 0.5;
+    this.generations++;
+    this.production = newProduction;
+}
+
+//convert production string to a turtle graphic
+PenroseLSystem.prototype.render = function () {
+    translate(width/2, height/2);
+
+    this.steps += 20;
+    if(this.steps > this.production.length) {
+      this.steps = this.production.length;
+    }
+
+    for(var i=0; i<this.steps; ++i) {
+      var step = this.production.charAt(i);
+
+      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
+      if( step == 'F') {
+        stroke(255, 60);
+        for(var j=0; j < this.repeats; j++) {
+          line(0, 0, 0, -this.drawLength);
+          noFill();
+          translate(0, -this.drawLength);
+        }
+        this.repeats = 1;
+      }
+      else if (step == '+') {
+        rotate(this.theta);
+      }
+      else if (step == '-') {
+        rotate(-this.theta);
+      }
+      else if (step == '[') {
+        push();
+      }
+      else if (step == ']') {
+        pop();
+      }
+    }
+  }
